e66aef96302b850c7b5d9b6a6767dbcf4c6eaf4d
Jamie Bliss <jamie@ivyleav.es>
2024-12-22 00:08:22 -0500

    Initial implementation

--- README.md
+++ README.md
@@ -3,3 +3,5 @@
 Sets a bunch of environment variables for uploading to Teahouse's S3.
 
 You'll need a separate step to actually do the upload.
+
+Requires the `id-token: write` permission.
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const core = require('@actions/core')
 const github = require('@actions/github')
+const http_client = require('@actions/http-client')
 
 /**
  * The main function for the action.
@@ -7,18 +8,42 @@ const github = require('@actions/github')
  */
 async function run() {
   try {
-    // The `who-to-greet` input is defined in action metadata file
-    const whoToGreet = core.getInput('who-to-greet', { required: true })
-    core.info(`Hello, ${whoToGreet}!`)
+    // The `domain` input is defined in action metadata file
+    const domain = core.getInput('domain', { required: true })
+    const server = core.getInput('server') || 'https://admin.teahouse.cafe'
+    core.debug('Getting OIDC token')
+    const oidc_token = await core.getIDToken()
 
-    // Get the current time and set as an output
-    const time = new Date().toTimeString()
-    core.setOutput('time', time)
+    core.info(`Configuring upload for ${domain}`)
+    const http = http_client.HttpClient()
+    const resp = await http.postJson(`${server}/upload/get-s3-config`, {
+      token: oidc_token,
+      domain
+    })
+    const envvars = resp.result
 
-    // Output the payload for debugging
-    core.info(
-      `The event payload: ${JSON.stringify(github.context.payload, null, 2)}`
-    )
+    if (envvars.AWS_ACCESS_KEY_ID) {
+      core.exportVariable('AWS_ACCESS_KEY_ID', envvars.AWS_ACCESS_KEY_ID)
+    }
+    if (envvars.AWS_ENDPOINT_URL_S3) {
+      core.exportVariable('AWS_ENDPOINT_URL_S3', envvars.AWS_ENDPOINT_URL_S3)
+    }
+    if (envvars.AWS_REGION) {
+      core.exportVariable('AWS_REGION', envvars.AWS_REGION)
+    }
+    if (envvars.AWS_SECRET_ACCESS_KEY) {
+      core.setSecret(envvars.AWS_SECRET_ACCESS_KEY)
+      core.exportVariable(
+        'AWS_SECRET_ACCESS_KEY',
+        envvars.AWS_SECRET_ACCESS_KEY
+      )
+    }
+    if (envvars.AWS_SESSION_TOKEN) {
+      core.exportVariable('AWS_SESSION_TOKEN', envvars.AWS_SESSION_TOKEN)
+    }
+    if (envvars.BUCKET_NAME) {
+      core.setOutput('bucket', envvars.BUCKET_NAME)
+    }
   } catch (error) {
     // Fail the workflow step if an error occurs
     core.setFailed(error.message)

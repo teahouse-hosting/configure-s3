b0e14c0b8f89abf62ebbfe019d32d94a43f7aa40
Christina Kidwell <43004589+kidwellc@users.noreply.github.com>
2025-01-12 17:55:35 -0500

    Tried to convert to ESM Modified config files main tests do not work

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,7 +1,6 @@
 /**
  * Unit tests for the action's entrypoint, src/index.js
  */
-
 const { run } = require('../src/main')
 
 // Mock the action's entrypoint
--- src/main.js
+++ src/main.js
@@ -1,12 +1,12 @@
-const core = require('@actions/core')
-const github = require('@actions/github')
+import * as core from '@actions/core'
+import * as github from '@actions/github'
 const http_client = require('@actions/http-client')
 
 /**
  * The main function for the action.
  * @returns {Promise<void>} Resolves when the action is complete.
  */
-async function run() {
+export async function run() {
   try {
     // The `domain` input is defined in action metadata file
     const domain = core.getInput('domain', { required: true })
@@ -51,8 +51,4 @@ async function run() {
     // Fail the workflow step if an error occurs
     core.setFailed(error.message)
   }
-}
-
-module.exports = {
-  run
 }

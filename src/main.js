e5c4cdd4497500ae7f7b3439f12232cc31e930bf
Jamie Bliss <jamie@ivyleav.es>
2025-04-04 20:23:40 -0400

    Try to print stack trace

--- src/main.js
+++ src/main.js
@@ -51,6 +51,7 @@ async function run() {
       core.setOutput('bucket', envvars.BUCKET_NAME)
     }
   } catch (error) {
+    console.trace("Error in configuring")
     // Fail the workflow step if an error occurs
     core.setFailed(error.message)
   }
